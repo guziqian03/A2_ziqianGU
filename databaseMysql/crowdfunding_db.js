894e869f0e5f4ffa594369c7b6e402b3557a55b6
guziqian03 <z.gu.13@student.scu.edu.au>
2024-09-17 17:49:43 +0800

    Create crowdfunding_db.js

---
+++ databaseMysql/crowdfunding_db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',  
+    password : 'Guziqian030121',
+    database : 'crowdfunding_db'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to the database!");
+});
+
+
+connection.end();
