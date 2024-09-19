699bd86faa2f369b201747803bcf3ca1c2bcbf71
guziqian03 <z.gu.13@student.scu.edu.au>
2024-09-19 13:39:59 +0800

    new

--- crowdfundingAPI/app.js
+++ crowdfundingAPI/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// 路由模块
+
 const fundraiserRoutes = require('./routes/fundraisers');
 
-// 中间件
+
 app.use(express.json());
 
-// 使用路由
+
 app.use('/api/fundraisers', fundraiserRoutes);
 
 app.listen(port, () => {
