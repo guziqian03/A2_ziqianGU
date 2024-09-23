b2402395ae4d955c9bada5707432dc50d62a4d71
guziqian03 <z.gu.13@student.scu.edu.au>
2024-09-23 13:17:05 +0800

    new

--- crowdfundingAPI/categories.js
+++ crowdfundingAPI/categories.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
-// 获取所有分类
 router.get('/', (req, res) => {
     db.query('SELECT * FROM CATEGORY', (error, results) => {
         if (error) throw error;
