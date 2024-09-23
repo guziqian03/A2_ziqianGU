787c5365b467dd1c3f0e064716a5e08681cd0c76
guziqian03 <z.gu.13@student.scu.edu.au>
2024-09-23 13:27:23 +0800

    pt3 update

--- crowdfundingAPI/server.js
+++ crowdfundingAPI/server.js
@@ -1,15 +1,19 @@
 var express = require('express');
+var cors = require('cors');  
 
 var app = express();
 
 var crowdfundingAPI = require("./controllerAPI/api-controller");
 
-var bodyparser=require("body-parser");
+var bodyparser = require("body-parser");
+
+app.use(cors());
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
-
+app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use("/api", crowdfundingAPI);
-app.listen(3001);
-console.log("Server up and running on port 3001");
+
+app.listen(3000, () => {
+    console.log("Server up and running on port 3000");
+});
